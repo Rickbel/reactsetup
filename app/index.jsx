a5e480532a9f47fdcff8ab7246c6f671d2c8b47f
Ricardo Humerez <rickbel_94@outlook.com>
2018-08-21 00:14:36 -0400

    segundo commit cambio de h1

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-const App = () => <h1>Hola amiguitos como estan </h1>
+const App = () => <h1>Hello react dev </h1>
 
 render(
     <App/>,document.getElementById('app')
